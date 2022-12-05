9598a4e7fb248b3fcd0583ebe2388d48763a9fd1
hami-r <mynameissaitama54@gmail.com>
2022-12-05 17:50:12 +0530

    homepage

---
+++ src/main/java/com/nest/libraryapp_backend/collector/LibraryCollector.java
@@ -0,0 +1,13 @@
+package com.nest.libraryapp_backend.collector;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibraryCollector {
+
+    @PostMapping("/")
+    public String homepage() {
+        return "Welcome to my website";
+    }
+}
